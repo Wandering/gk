07aaa30587f150dddd3a27ceacbba2bc8af0f98d
王永海 <yhwang@thinkjoy.cn>
2015-09-24 12:01:05 +0800

    完成课程

---
+++ before-service/src/main/java/cn/thinkjoy/gk/dao/ex/IEXPaperDAO.java
@@ -0,0 +1,20 @@
+package cn.thinkjoy.gk.dao.ex;
+
+import cn.thinkjoy.gk.domain.ExaminationPaper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * Created by yhwang on 15/9/24.
+ */
+public interface IEXPaperDAO {
+    /**
+     * 分页获取试卷
+     * @param sortType
+     * @param offSet
+     * @param rows
+     * @return
+     */
+    List<ExaminationPaper> getPaperPage(@Param("sortType")Integer sortType ,@Param("years")String years,@Param("offSet") Integer offSet, @Param("rows")Integer rows);
+}
