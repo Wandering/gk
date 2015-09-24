34d86f913966d3e75c7dec30ccda0dd60ad7ad20
李金泽 <jzli@thinkjoy.cn>
2015-09-24 16:59:27 +0800

    志愿学堂搜索

--- gk-war/src/main/java/cn/thinkjoy/gk/controller/guide/VolunteerSchoolController.java
+++ gk-war/src/main/java/cn/thinkjoy/gk/controller/guide/VolunteerSchoolController.java
@@ -7,10 +7,13 @@ import cn.thinkjoy.gk.domain.VolunteerSchoolCategory;
 import cn.thinkjoy.gk.service.IVolunteerSchoolCategoryService;
 import cn.thinkjoy.gk.service.IVolunteerSchoolService;
 import com.google.common.collect.Maps;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.Map;
 
@@ -27,7 +30,7 @@ public class VolunteerSchoolController {
     private IVolunteerSchoolService volunteerSchoolService;
 
     /** 志愿学堂分类 */
-    @RequestMapping(value = "/categories")
+    @RequestMapping(value = "/categories", method = RequestMethod.GET)
     @ResponseBody
     public List<VolunteerSchoolCategory> getCategorys() {
         Map<String, Object> conditions = Maps.newHashMap();
@@ -37,15 +40,24 @@ public class VolunteerSchoolController {
     /** 志愿学堂文章列表 */
     @RequestMapping(value = "/articles", method = RequestMethod.GET)
     @ResponseBody
-    public BizData4Page<VolunteerSchool> getArticles(@RequestParam("cateId") long cateId, @RequestParam("pn") int pn,
+    public BizData4Page<VolunteerSchool> getArticles(HttpServletRequest request, @RequestParam("pn") int pn,
                                                      @RequestParam("ps") int ps, @RequestParam("lastId") long lastId) {
+        long cateId = ServletRequestUtils.getLongParameter(request, "cateId", 0);
+        String keyword = request.getParameter("kw");
         if (pn == 0) pn = 1;
         if (ps == 0) ps = 10;
         Map<String, Object> conditions = Maps.newHashMap();
-        SearchField category = new SearchField();
-        category.setField("categoryId");
-        category.setOp("=");
-        category.setData(cateId+"");
+        if (StringUtils.isNotBlank(keyword)) {
+            conditions.put("keyword", keyword);
+        }
+        if (cateId > 0) {
+            SearchField category = new SearchField();
+            category.setField("categoryId");
+            category.setOp("=");
+            category.setData(cateId + "");
+
+            conditions.put(category.getField(), category);
+        }
         SearchField status = new SearchField();
         status.setField("status");
         status.setOp("=");
@@ -54,14 +66,14 @@ public class VolunteerSchoolController {
         beginId.setField("id");
         beginId.setOp(">");
         beginId.setData(lastId + "");
-        conditions.put(category.getField(), category);
+
         conditions.put(status.getField(), status);
         conditions.put(beginId.getField(), beginId);
         conditions.put("groupOp", "and");
         return volunteerSchoolService.queryPageByDataPerm(null, conditions, pn, (pn-1) * ps, ps);
     }
 
-    @RequestMapping(value = "/article")
+    @RequestMapping(value = "/article", method = RequestMethod.GET)
     @ResponseBody
     public VolunteerSchool getArticleDetial(@RequestParam("id") long id) {
         return (VolunteerSchool) volunteerSchoolService.fetch(id);
