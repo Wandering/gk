955e2064223cb4fc35f8bfdff5bd32f8b1caa019
雷超 <clei@thinkjoy.cn>
2015-09-24 14:40:53 +0800

    更新代码

--- gk-war/src/main/java/cn/thinkjoy/gk/util/IPUtil.java
+++ gk-war/src/main/java/cn/thinkjoy/gk/util/IPUtil.java
@@ -25,6 +25,8 @@ public class IPUtil {
 		String ip = request.getHeader("x-forwarded-for");
 		if(ip!=null){
 			ip = ip.split(",")[0].trim();
+		}else{
+			ip = "127.0.0.1";
 		}
 		return ip;
 	}   
