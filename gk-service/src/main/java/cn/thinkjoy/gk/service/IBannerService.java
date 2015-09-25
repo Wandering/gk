2a6c8eb05d31be6d31b5ee33c397c8d4fc900362
zhanglun <zhanglun@thinkjoy.cn>
2015-09-25 11:09:55 +0800

    gkbanner

--- gk-service/src/main/java/cn/thinkjoy/gk/service/IBannerService.java
+++ gk-service/src/main/java/cn/thinkjoy/gk/service/IBannerService.java
@@ -2,9 +2,11 @@ package cn.thinkjoy.gk.service;
 
 import cn.thinkjoy.gk.domain.Banner;
 
+import java.util.List;
+
 /**
  * Created by ZL on 2015/9/23.
  */
 public interface IBannerService {
-    public Banner getBannerByType(Integer type);
+    public List<Banner> getBannerByType(Integer type);
 }
