3d226da900f58d67cfc85648bad9e5b1964021aa
pdeng <pdeng@thinkjoy.cn>
2015-09-25 11:24:45 +0800

    首页

--- gk-war/src/main/webapp/static/bower_components/utils/swiper.js
+++ gk-war/src/main/webapp/static/bower_components/utils/swiper.js
@@ -1,5 +1,4 @@
 define(function (require) {
-
     var $ = require('$');
     require('swiper-jquery');
     //幻灯片
@@ -10,29 +9,24 @@ define(function (require) {
         prevButton: '.swiper-button-prev',
         loop: true
     });
-
-    //$.ajax({
-    //    url: '/banner/getUrlByType.do',
-    //    type: 'get',
-    //    dataType: 'json',
-    //    data: {
-    //        type:1
-    //    },
-    //    success: function (res) {
-    //        console.log(res);
-    //        if (res.rtnCode == '0000000') {
-    //            //window.location.href
-    //        }
-    //    }
-    //});
-
-    $.getJSON('/banner/getUrlByType.do',{
-        type:1
-    },function(res){
-        console.log(res);
-    })
-
-
+    $.ajax({
+        url: '/banner/getUrlByType.do',
+        type: 'get',
+        dataType: 'json',
+        data: {
+            type: 1
+        },
+        success: function (res) {
+            console.log(res);
+            if (res.rtnCode == '0000000') {
+                var dataJson = res.bizData;
+                $.each(dataJson, function (i, v) {
+                    console.log(v.imageUrl)
+                    //$('.banner-toggle').attr('src',v);
+                })
+            }
+        }
+    });
 
 
 });
