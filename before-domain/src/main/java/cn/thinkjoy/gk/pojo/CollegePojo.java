104eb74aab1f2db6962c8135c0d3caf9f3db56a6
王永海 <yhwang@thinkjoy.cn>
2015-09-28 15:36:37 +0800

    院校推荐

---
+++ before-domain/src/main/java/cn/thinkjoy/gk/pojo/CollegePojo.java
@@ -0,0 +1,28 @@
+package cn.thinkjoy.gk.pojo;
+
+import java.io.Serializable;
+
+/**
+ * Created by yhwang on 15/9/28.
+ */
+public class CollegePojo implements Serializable{
+
+    private Long m_university_code;//学校编码
+    private String m_university_name;//学校名称
+
+    public Long getM_university_code() {
+        return m_university_code;
+    }
+
+    public void setM_university_code(Long m_university_code) {
+        this.m_university_code = m_university_code;
+    }
+
+    public String getM_university_name() {
+        return m_university_name;
+    }
+
+    public void setM_university_name(String m_university_name) {
+        this.m_university_name = m_university_name;
+    }
+}
