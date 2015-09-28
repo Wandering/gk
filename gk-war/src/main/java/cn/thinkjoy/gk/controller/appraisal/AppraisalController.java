7d91ec3e6ca9e0079e087e42bb20670e2f5969b7
雷超 <clei@thinkjoy.cn>
2015-09-28 22:19:08 +0800

    更新代码

--- gk-war/src/main/java/cn/thinkjoy/gk/controller/appraisal/AppraisalController.java
+++ gk-war/src/main/java/cn/thinkjoy/gk/controller/appraisal/AppraisalController.java
@@ -15,9 +15,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import java.util.HashMap;
 import java.util.Map;
 
