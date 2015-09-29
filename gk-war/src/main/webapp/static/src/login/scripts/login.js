f700eafb51fecef710238597fd8e5cd6d4ed77e5
hzuo <hzuo@thinkjoy.cn>
2015-09-29 16:59:10 +0800

    注册成功后进入个人中心

--- gk-war/src/main/webapp/static/src/login/scripts/login.js
+++ gk-war/src/main/webapp/static/src/login/scripts/login.js
@@ -172,7 +172,8 @@ define(function (require) {
                 success: function (res) {
                     console.log(res);
                     if (res.rtnCode == '0000000') {
-                        window.location.href = 'http://'+window.location.host+'/login/login.jsp';
+                        window.location.assign('http://' + window.location.host + '/user/personal-info.jsp');
+                        //window.location.href = 'http://'+window.location.host+'/login/login.jsp';
                     }else{
                         Dom.errorTip2(res.msg);
                     }
