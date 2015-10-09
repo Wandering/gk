e67e1598e47a422e4bd5cbfa91e83a3bef27130a
dsfderek <dsfderek@163.com>
2015-10-09 18:12:14 +0800

    after

--- gk-war/src/main/webapp/static/src/after/scripts/after-flow3.js
+++ gk-war/src/main/webapp/static/src/after/scripts/after-flow3.js
@@ -1,37 +1,36 @@
 define(function (require) {
     var $ = require('$');
-    $(function(){
+    $(function () {
         //
-        $('.volunteer-flow3-table').on('click','.open-flow3',function(){
+        $('.volunteer-flow3-table').on('click', '.open-flow3', function () {
             $('.tansLayer,.volunteer-flow3-layer').show();
-            $('#volunteer-flow3-layer').attr('dataType',$(this).attr('dataType'))
+            $('#volunteer-flow3-layer').attr('dataType', $(this).attr('dataType'))
         });
 
 
-        $('#volunteer-flow3-layer').on('click','.close-btn',function(){
+        $('#volunteer-flow3-layer').on('click', '.close-btn', function () {
             $('#volunteer-flow3-layer,.tansLayer').hide();
         });
         //console.log(JSON.parse(params).m_province);
 
 
-
         var paramsJson = JSON.parse(params);
         var params1 = {
-            "m_candidateNumber":"0",
-            "m_aggregateScore":390,
-            "m_ranking":72465,
-            "m_kelei":"文史",
-            "m_batch_id":4,
-            "m_batch":"高职（专科）",
-            "m_province_id":"0",
-            "m_province":"",
-            "m_specialty_id":"",
-            "m_specialty_name":"",
-            "m_favorites_by_university_codes":""
+            "m_candidateNumber": "0",
+            "m_aggregateScore": 390,
+            "m_ranking": 72465,
+            "m_kelei": "文史",
+            "m_batch_id": 4,
+            "m_batch": "高职（专科）",
+            "m_province_id": "0",
+            "m_province": "",
+            "m_specialty_id": "",
+            "m_specialty_name": "",
+            "m_favorites_by_university_codes": ""
         };
 
-        getSchool(params1,"","");
-        function getSchool(paramsJson,m_province,m_specialty_name){
+        getSchool(params1, "", "");
+        function getSchool(paramsJson, m_province, m_specialty_name) {
             paramsJson.m_province = m_province;
             paramsJson.m_specialty_name = m_specialty_name;
             //console.log(paramsJson);
@@ -39,7 +38,7 @@ define(function (require) {
                 url: '/guide/school.do',
                 type: 'GET',
                 dataType: 'JSON',
-                data:paramsJson,
+                data: paramsJson,
                 success: function (res) {
                     //console.log(res);
                     if (res.rtnCode == "0100006" || res.rtnCode == "1000004" || res.rtnCode == "0100005") {
@@ -51,7 +50,7 @@ define(function (require) {
                         //console.log(data)
                         var m_batch_id = data.related.m_batch_id;
                         var listData = data.data.result;
-                        if(listData.data.length==0){
+                        if (listData.data.length == 0) {
                             $('.no-school').show();
                             $('.school-list').hide();
                         }
@@ -60,27 +59,27 @@ define(function (require) {
 
                         var m_keleiType = '';
                         //console.log(data.related.m_kelei)
-                        if(data.related.m_kelei=="文史"){
+                        if (data.related.m_kelei == "文史") {
                             m_keleiType = 0;
-                        }else if(data.related.m_kelei=="理工"){
+                        } else if (data.related.m_kelei == "理工") {
                             m_keleiType = 1;
                         }
 
                         $.each(listData.data, function (i, v) {
-                            if(listData.data[i].data.length==0){
+                            if (listData.data[i].data.length == 0) {
                                 $('.no-school').show();
                                 $('.school-list').hide();
                             }
-                            if(listData.data[i].data.length > 0 && i < 4){
-                                $('#no-school'+i).hide();
-                                $('#school-list'+i).show();
+                            if (listData.data[i].data.length > 0 && i < 4) {
+                                $('#no-school' + i).hide();
+                                $('#school-list' + i).show();
                                 $.each(listData.data[i].data, function (j, m) {
                                     var schoolListHtml = ''
-                                        +'<div>'
-                                        +'<span class="fl"><a target="_blank" href="/consult/school_detile.jsp?id='+ m.m_university_code +'&batch='+ m_batch_id +'" id="'+ m.m_university_code +'">'+ m.m_university_name +'</a></span>'
-                                        +'<span class="fr" id="'+ m.m_university_code +'" type = "'+ m_keleiType + '">选择</span>'
-                                        +'</div>';
-                                    $('#school-list'+i).append(schoolListHtml);
+                                        + '<div>'
+                                        + '<span class="fl"><a target="_blank" href="/consult/school_detile.jsp?id=' + m.m_university_code + '&batch=' + m_batch_id + '" id="' + m.m_university_code + '">' + m.m_university_name + '</a></span>'
+                                        + '<span class="fr" id="' + m.m_university_code + '" type = "' + m_keleiType + '">选择</span>'
+                                        + '</div>';
+                                    $('#school-list' + i).append(schoolListHtml);
                                 })
                             }
                         });
@@ -95,44 +94,55 @@ define(function (require) {
             //console.log(result)
             var data = result.bizData;
             if ('0000000' === result.rtnCode) {
-                $.each(data,function(i,v){
-                    var provinceName = v.name.replace(/[省,市,自治区]/g,"");
+                $.each(data, function (i, v) {
+                    var provinceName = v.name.replace(/[省,市,自治区]/g, "");
                     //console.log(provinceName)
-                    $('#province-list').append('<option value="'+ provinceName +'">' + provinceName + '</option>')
+                    $('#province-list').append('<option value="' + provinceName + '">' + provinceName + '</option>')
                 });
             }
         });
 
         // 省份 专业查询
-         $('#specialty-search-btn').on('click',function(){
-             var specialtyV = $('#specialty-input').val().trim();
-             var provinceV = $("#province-list option:selected").val();
-             getSchool(params1,provinceV,specialtyV);
-         });
-
-
-
-
+        $('#specialty-search-btn').on('click', function () {
+            var specialtyV = $('#specialty-input').val().trim();
+            var provinceV = $("#province-list option:selected").val();
+            getSchool(params1, provinceV, specialtyV);
+        });
 
 
         $.ajax({
             url: '/university/universityDetail.do',
             type: 'GET',
             dataType: 'JSON',
-            data:{
-                code:2406,
-                type:0,
-                batch:"高职（专科）"
+            data: {
+                code: 2406,
+                type: 0,
+                year: 2014,
+                batch: "高职（专科）"
             },
             success: function (res) {
                 console.log(res);
+                var data = res.bizData;
+                if ('0000000' === result.rtnCode) {
+                    var infoHtml = ''
+                        + '<p>'
+                        + '院校代码：'+ data.code +'  <br/>'
+                        + '院校特征：211 研<br/>'
+                        + '院校隶属：'+ data.subjection +'<br/>'
+                        + '院校类型：'+ data.type +'<br/>'
+                        + '2014年最低投档分：'+ data.lowestScore +'<br/>'
+                        + '2014年最低位次：'+ data.lowestRanking +' <br/>'
+                        + '2014年录取平均分：'+ data.averageScore +' <br/>'
+                        + '2014年平均分位次：'+ data.averageScoresRanking +' <br/>'
+                        + '历年招生情况：'+ data.enrollIntro +' <br/>'
+                        + '录取指数：★★'
+                        + '</p>'
+                }
+
 
 
             }
         });
-
-
-
 
 
     })
