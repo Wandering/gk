21afeaf0ec36f1cafc8fe2d67cc73be63add8ae5
雷超 <clei@thinkjoy.cn>
2015-10-09 18:10:00 +0800

    Merge branch 'master' of http://git.thinkjoy.cn/scm/gk360/gk
    # Conflicts:
    #	consult-service/src/main/resources/mybatis/UniversityExDAO.xml

--- consult-domain/src/main/java/cn/thinkjoy/gk/pojo/UniversityDetailDto.java
+++ consult-domain/src/main/java/cn/thinkjoy/gk/pojo/UniversityDetailDto.java
@@ -11,6 +11,8 @@ public class UniversityDetailDto {
 
     private String subjection;
 
+    private String dictName;
+
     private String type;
 
     private Integer planNum;
@@ -107,5 +109,19 @@ public class UniversityDetailDto {
         this.averageScoresRanking = averageScoresRanking;
     }
 
+    public String getDictName() {
+        return dictName;
+    }
 
+    public void setDictName(String dictName) {
+        this.dictName = dictName;
+    }
+
+    public String getEnrollIntro() {
+        return enrollIntro;
+    }
+
+    public void setEnrollIntro(String enrollIntro) {
+        this.enrollIntro = enrollIntro;
+    }
 }
