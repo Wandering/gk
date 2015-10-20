d11afab53290a0c61ca88fc8eefd806707d28b73
雷超 <clei@thinkjoy.cn>
2015-10-20 16:41:14 +0800

    更新代码

--- web-common/src/main/java/cn/thinkjoy/gk/constant/DomainConst.java
+++ web-common/src/main/java/cn/thinkjoy/gk/constant/DomainConst.java
@@ -6,20 +6,24 @@ public class DomainConst {
 	 * 陕西
 	 */
 	public static final String SN_DOMAIN = "sn.gaokao360.net";
+	public static final String SN_DOMAIN_CODE = "610000";
 
 	/**
 	 * 湖南
 	 */
 	public static final String HN_DOMAIN = "hn.gaokao360.net";
+	public static final String HN_DOMAIN_CODE = "110100";
 
 	/**
 	 * 河南
 	 */
 	public static final String HA_DOMAIN = "ha.gaokao360.net";
+	public static final String HA_DOMAIN_CODE = "210100";
 
 	/**
 	 * 广西
 	 */
 	public static final String GX_DOMAIN = "gx.gaokao360.net";
+	public static final String GX_DOMAIN_CODE = "410000";
 
 }
