7895f5571fac3971bc727b653bb725914ff1ff7b
雷超 <clei@thinkjoy.cn>
2015-10-23 15:33:30 +0800

    更新代码

---
+++ user-service/src/main/java/cn/thinkjoy/gk/service/IUserExamService.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2013-2014, thinkjoy Inc. All Rights Reserved.
+ *
+ * Project Name: shishuo
+ * $Id:  UserExamService.java 2015-10-23 14:44:11 $
+ */
+
+package cn.thinkjoy.gk.service;
+import cn.thinkjoy.common.dao.IBaseDAO;
+import cn.thinkjoy.common.domain.BaseDomain;
+import cn.thinkjoy.common.service.IBaseService;
+import cn.thinkjoy.common.service.IPageService;
+
+public interface IUserExamService<D extends IBaseDAO<T>, T extends BaseDomain> extends IBaseService<D, T>,IPageService<D, T>{
+
+}
