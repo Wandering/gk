5dcc83d1cf96f3249d4a423720faa9ba5f52956e
hzuo <hzuo@thinkjoy.cn>
2015-10-23 17:36:12 +0800

    修复院校推荐bug

--- gk-sn-war/src/main/java/cn/thinkjoy/gk/controller/appraisal/CollegeRecController.java
+++ gk-sn-war/src/main/java/cn/thinkjoy/gk/controller/appraisal/CollegeRecController.java
@@ -27,11 +27,11 @@ public class CollegeRecController extends BaseController {
     public String GetCollegeList(@RequestParam(value="m_aggregateScore",required=false) String m_aggregateScore,
                                    @RequestParam(value="m_batch",required=false) String m_batch,
                                            @RequestParam(value="m_kelei",required=false) String m_kelei,
-                                           @RequestParam(value="id",required=false) String id){
+                                           @RequestParam(value="code",required=false) String code){
         if(StringUtils.isBlank(m_aggregateScore)
                 || StringUtils.isBlank(m_batch)
                 || StringUtils.isBlank(m_kelei)
-                || StringUtils.isBlank(id)){
+                || StringUtils.isBlank(code)){
             throw new BizException(ERRORCODE.PARAM_ISNULL.getCode(),ERRORCODE.PARAM_ISNULL.getMessage());
         }
 
@@ -49,13 +49,13 @@ public class CollegeRecController extends BaseController {
 
         Long value = userAccountPojo.getId();
 
-        Object resultId = session.getAttribute(VerificationKeyConst.COLLEGE_RECOMMENDATION+value);
+        Object resultCode = session.getAttribute(VerificationKeyConst.COLLEGE_RECOMMENDATION+value);
 
-        if(resultId==null){
+        if(resultCode==null){
             throw new BizException(ERRORCODE.VERIFY_CODE_ERROR.getCode(),ERRORCODE.VERIFY_CODE_ERROR.getMessage());
         }
 
-        if(!resultId.toString().equals(id.toUpperCase())){
+        if(!resultCode.toString().equals(code.toUpperCase())){
             throw new BizException(ERRORCODE.VERIFY_CODE_ERROR.getCode(),ERRORCODE.VERIFY_CODE_ERROR.getMessage());
         }
 
