47086b4de052d115a620c5ba7d7d0797b40f65f4
dsfderek <dsfderek@163.com>
2015-10-26 11:15:47 +0800

    commit

---
+++ gk-gx-war/src/main/webapp/static/bower_components/utils/getTimes.js
@@ -0,0 +1,34 @@
+// 转化时间戳
+define(function (require, exports, module) {
+    Date.prototype.Format = function (fmt) { //author: meizz
+        console.log(fmt);
+        var o = {
+            "M+": this.getMonth() + 1, //月份
+            "d+": this.getDate(), //日
+            "h+": this.getHours(), //小时
+            "m+": this.getMinutes(), //分
+            "s+": this.getSeconds(), //秒
+            "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+            "S": this.getMilliseconds() //毫秒
+        };
+        if (/(y+)/.test(fmt))
+            fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        for (var k in o)
+            if (new RegExp("(" + k + ")").test(fmt))
+                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        return fmt;
+    };
+    function getTime(timestamp, str) {
+        console.log(timestamp);
+        console.log(new Date(timestamp));
+        return new Date(timestamp).Format(str || 'yyyy-MM-dd');  //yyyy-MM-dd
+    }
+
+    function getTime1(timestamp, str) {
+        return new Date(timestamp).Format(str || 'MM-dd');  //yyyy-MM-dd
+    }
+
+    module.exports = function(timestamp, str) {
+        getTime(timestamp, str);
+    };
+});
