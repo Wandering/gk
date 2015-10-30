248622a634bf01e4e803b709358ab54fd77965b2
雷超 <clei@thinkjoy.cn>
2015-10-30 15:49:01 +0800

    更新代码

--- gk-user-web/src/main/java/cn/thinkjoy/gk/controller/VipController.java
+++ gk-user-web/src/main/java/cn/thinkjoy/gk/controller/VipController.java
@@ -8,6 +8,7 @@ import cn.thinkjoy.gk.domain.UserVip;
 import cn.thinkjoy.gk.pojo.CardPojo;
 import cn.thinkjoy.gk.pojo.UserAccountPojo;
 import cn.thinkjoy.gk.protocol.ERRORCODE;
+import cn.thinkjoy.gk.service.ICardExService;
 import cn.thinkjoy.gk.service.ICardService;
 import cn.thinkjoy.gk.service.IUserVipService;
 import org.slf4j.Logger;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,9 +31,9 @@ public class VipController extends BaseController {
 
     private static final Logger LOGGER= LoggerFactory.getLogger(VipController.class);
     @Autowired
-    private IUserVipService vipService;
-    @Autowired
     private ICardService cardService;
+    @Autowired
+    private ICardExService cardExService;
 
     @RequestMapping(value = "/upgradeVipByCard",method = RequestMethod.POST)
     @ResponseBody
@@ -40,11 +42,9 @@ public class VipController extends BaseController {
         if(null==userAccountPojo){
             throw new BizException(ERRORCODE.USER_NO_EXIST.getCode(), ERRORCODE.USER_NO_EXIST.getMessage());
         }
-
         Integer vipStatus = userAccountPojo.getVipStatus();
-        if(userAccountPojo.getVipStatus().intValue()==1){
+        if(null!=vipStatus&&vipStatus==1){
             throw new BizException(ERRORCODE.VIP_EXIST.getCode(), ERRORCODE.VIP_EXIST.getMessage());
-
         }
         long areaId=getAreaCookieValue();
         Map<String,Object> map=new HashMap<String, Object>();
@@ -54,24 +54,32 @@ public class VipController extends BaseController {
         Card card=(Card)cardService.queryOne(map);
         if(null==card ){
             throw new BizException(ERRORCODE.VIP_CARD_NOT_INVALID.getCode(), ERRORCODE.VIP_CARD_NOT_INVALID.getMessage());
-        }else if(0!=card.getUserId()){
+        }
+        if(card.getUserId()!=0){
             throw new BizException(ERRORCODE.VIP_CARD_NOT_INVALID.getCode(), ERRORCODE.VIP_CARD_NOT_INVALID.getMessage());
         }
-        card.setUserId(userAccountPojo.getId());
-        cardService.update(card);
-        UserVip userVip=new UserVip();
-        userVip.setId(userAccountPojo.getId());
-        userVip.setEndDate(card.getEndDate());
-        userVip.setStatus(1);
-        vipService.update(userVip);
-
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(System.currentTimeMillis());
+        c.add(Calendar.YEAR, 1);
+        c.set(Calendar.MONTH, 8);
+        c.set(Calendar.DAY_OF_MONTH, 1);
+        if(card.getEndDate()<=c.getTimeInMillis()){
+            throw new BizException(ERRORCODE.VIP_CARD_NOT_INVALID.getCode(), ERRORCODE.VIP_CARD_NOT_INVALID.getMessage());
+        }
+        cardExService.updateUserVip(card.getId(),userAccountPojo.getId());
         userAccountPojo.setVipStatus(1);
-
         try {
             setUserAccountPojo(userAccountPojo);
         }catch(Exception e){
             throw new BizException(ERRORCODE.FAIL.getCode(), ERRORCODE.FAIL.getMessage());
         }
+
+//        新增卡号的时候添加
+//        Calendar c1 = Calendar.getInstance();
+//        c1.setTimeInMillis(System.currentTimeMillis());
+//        c1.add(Calendar.YEAR, 1);
+//        c1.set(Calendar.MONTH, 8);
+//        c1.set(Calendar.DAY_OF_MONTH, 1);
 
         return  "success";
     }
