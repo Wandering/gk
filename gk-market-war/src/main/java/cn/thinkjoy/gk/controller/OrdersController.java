4a6b87b1385ce1b87b192b9325547ef65456be76
雷超 <clei@thinkjoy.cn>
2015-11-04 10:22:51 +0800

    更新代码

--- gk-market-war/src/main/java/cn/thinkjoy/gk/controller/OrdersController.java
+++ gk-market-war/src/main/java/cn/thinkjoy/gk/controller/OrdersController.java
@@ -115,7 +115,7 @@ public class OrdersController extends BaseController {
         order.setPayStatus(0);
         order.setDescription(ordersQuery.getExtra());
 //        order.setChannel(ordersQuery.getChannel());
-        order.setChannel("alipay_pc_direct");
+        order.setChannel(ordersQuery.getChannel());
         try {
             ordersService.insert(order);
             LOGGER.info("creaate order :" + orderNo);
