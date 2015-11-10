ebc59116f5643a0cec9bf39bf70c6901d7cee170
雷超 <clei@thinkjoy.cn>
2015-11-10 19:45:05 +0800

    更新代码

---
+++ consult-service/src/main/java/cn/thinkjoy/gk/dao/IMajoredRankDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2013-2014, thinkjoy Inc. All Rights Reserved.
+ *
+ * Project Name: timer
+ * $Id:  MajoredRankDAO.java 2015-11-10 15:57:32 $
+ */
+package cn.thinkjoy.gk.dao;
+
+import cn.thinkjoy.common.dao.IBaseDAO;
+import cn.thinkjoy.gk.domain.MajoredRank;
+
+public interface IMajoredRankDAO extends IBaseDAO<MajoredRank>{
+	
+
+}
--- gk-upload-war/src/main/java/cn/thinkjoy/gk/constant/VideoConst.java
+++
@@ -1,7 +0,0 @@
-package cn.thinkjoy.gk.constant;
-
-public final class VideoConst {
-
-	public static final String TYPE = "mp4";
-
-}
