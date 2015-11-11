0fa1f85ddb2f7094ffc06af87836ffea7a4034ce
雷超 <clei@thinkjoy.cn>
2015-11-11 14:52:27 +0800

    更新代码

--- consult-domain/src/main/java/cn/thinkjoy/gk/pojo/MajoredRankDto.java
+++ consult-domain/src/main/java/cn/thinkjoy/gk/pojo/MajoredRankDto.java
@@ -29,7 +29,72 @@ public class MajoredRankDto extends MajoredRank{
 
     private Integer isOpen;
 
+    @Override
+    public String getUniversityName() {
+        return universityName;
+    }
 
+    @Override
+    public void setUniversityName(String universityName) {
+        this.universityName = universityName;
+    }
 
+    @Override
+    public String getMajorName() {
+        return majorName;
+    }
+
+    @Override
+    public void setMajorName(String majorName) {
+        this.majorName = majorName;
+    }
+
+    public Integer getEnrollNumber() {
+        return enrollNumber;
+    }
+
+    public void setEnrollNumber(Integer enrollNumber) {
+        this.enrollNumber = enrollNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getFeeStandard() {
+        return feeStandard;
+    }
+
+    public void setFeeStandard(String feeStandard) {
+        this.feeStandard = feeStandard;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getForeignLanguage() {
+        return foreignLanguage;
+    }
+
+    public void setForeignLanguage(String foreignLanguage) {
+        this.foreignLanguage = foreignLanguage;
+    }
+
+    public Integer getIsOpen() {
+        return isOpen;
+    }
+
+    public void setIsOpen(Integer isOpen) {
+        this.isOpen = isOpen;
+    }
 }
 
