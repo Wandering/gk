a961d67446540b9fd00437e8016453330a78eea3
hzuo <hzuo@thinkjoy.cn>
2015-11-25 09:26:03 +0800

    优化高考热点列表获取

--- gk-service/src/main/java/cn/thinkjoy/gk/dao/IGkinformationGkhotDAO.java
+++ gk-service/src/main/java/cn/thinkjoy/gk/dao/IGkinformationGkhotDAO.java
@@ -12,6 +12,15 @@ public interface IGkinformationGkhotDAO{
     public List<GkinformationGkhot> getAllInformation(@Param("areaId")long areaId,@Param("offset")Integer offset,@Param("rows")Integer rows);
     public List<GkinformationGkhot> getInformationByKey(@Param("areaId")long areaId,@Param("key") String key,@Param("offset")Integer offset,@Param("rows")Integer rows);
     public GkinformationGkhot getInformationContentById(@Param("id") Integer id);
+
+    /**
+     * 获取高考热点列表
+     * 由于列表主要为标题时间，正文内容过大，不做查询
+     * @param areaId
+     * @param offset
+     * @param rows
+     * @return
+     */
     public List<GkinformationGkhot> getHotInformation(@Param("areaId")long areaId,@Param("offset")Integer offset,@Param("rows")Integer rows);
     int updateHotCount(Integer id);
     int saveGkinformationGkhot(GkinformationGkhot gkinformationGkhot);
