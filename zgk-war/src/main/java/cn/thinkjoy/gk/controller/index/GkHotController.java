4d3a481f64d75d67c443cb3d5cb4300c3fdd2d7b
yyp <wuhun0301@qq.com>
2016-01-05 17:12:46 +0800

    添加接口

--- zgk-war/src/main/java/cn/thinkjoy/gk/controller/index/GkHotController.java
+++ zgk-war/src/main/java/cn/thinkjoy/gk/controller/index/GkHotController.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -44,7 +45,7 @@ public class GkHotController {
      */
     @RequestMapping(value = "/getGkHotInfo",method = RequestMethod.GET)
     @ResponseBody
-    public GkHot getGkHotInfo(String id){
+    public GkHot getGkHotInfo(@RequestParam("id")String id){
         return gkHotService.getGkHotInfo(id);
     }
 }
--- zgk-war/src/main/java/cn/thinkjoy/gk/controller/index/GkScheduleController.java
+++ zgk-war/src/main/java/cn/thinkjoy/gk/controller/index/GkScheduleController.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -45,7 +46,7 @@ public class GkScheduleController {
      */
     @RequestMapping(value = "/getScheduleInfo",method = RequestMethod.GET)
     @ResponseBody
-    public GkSchedule getScheduleInfo(String id){
+    public GkSchedule getScheduleInfo(@RequestParam("id")String id){
         return gkScheduleService.getScheduleInfo(id);
     }
 }
