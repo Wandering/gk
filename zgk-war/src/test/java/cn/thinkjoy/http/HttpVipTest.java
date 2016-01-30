2851e5442b514c8eb83b487475e6fff77d37f87e
yyp <wuhun0301@qq.com>
2016-01-30 17:17:08 +0800

    升级vipbug

---
+++ zgk-war/src/test/java/cn/thinkjoy/http/HttpVipTest.java
@@ -0,0 +1,26 @@
+package cn.thinkjoy.http;
+
+import cn.thinkjoy.common.RequestUtils;
+import junit.framework.TestCase;
+
+/**
+ * Created by admin on 2016/1/14.
+ */
+public class HttpVipTest extends TestCase{
+//        String host="http://10.254.130.33:9080";
+//        String host="http://zjtest.zhigaokao.cn";
+        String host="http://localhost:8080";
+        String upgradeVipByCard_url="/vip/upgradeVipByCard.do";
+
+    /**
+     * 升级VIP
+     */
+     public void testGetPerformanceDetail(){
+            String url=host + upgradeVipByCard_url + "?cardNumber=0311240868&password=286940616&token=Na5QqNTNBAbNjFm%2FryG3YQ%3D%3D&userKey=zj\n";
+            String result = RequestUtils.requestPost(url);
+            assertTrue(result.contains("33"));
+     }
+
+
+
+}
