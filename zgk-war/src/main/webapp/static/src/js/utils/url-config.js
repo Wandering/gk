efd32e231873cad2ee9d2a88ebb8a653301849d3
邓少飞 <sfdeng@thinkjoy.cn>
2016-01-30 21:09:13 +0800

    commit 1

--- zgk-war/src/main/webapp/static/src/js/utils/url-config.js
+++ zgk-war/src/main/webapp/static/src/js/utils/url-config.js
@@ -3,8 +3,8 @@
  * */
 //url接口地址
 //var BASE_URL = 'http://10.254.130.33:9080';
-//var BASE_URL = 'http://172.16.130.99:8080';
-var BASE_URL = '';
+var BASE_URL = 'http://10.136.21.171:8080';
+//var BASE_URL = '';
 var interfaceUrl = {
     /*
      * 高考咨询
