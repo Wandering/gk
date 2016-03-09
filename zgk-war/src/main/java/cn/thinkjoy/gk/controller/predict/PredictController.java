3f6b73125f76e1087604176dd73c005bedb49b44
yyp <wuhun0301@qq.com>
2016-03-09 15:23:37 +0800

    目标定位异常

--- zgk-war/src/main/java/cn/thinkjoy/gk/controller/predict/PredictController.java
+++ zgk-war/src/main/java/cn/thinkjoy/gk/controller/predict/PredictController.java
@@ -129,7 +129,7 @@ public class PredictController extends BaseApiController {
         //判断是否今天定位过
         boolean flag = userInfoExService.isPredictByUid(Long.parseLong(this.getAccoutId()));
         if(!flag){
-//            throw new BizException(ERRORCODE.HASPREDICT.getCode(),ERRORCODE.HASPREDICT.getMessage());
+            throw new BizException(ERRORCODE.HASPREDICT.getCode(),ERRORCODE.HASPREDICT.getMessage());
         }
         //end
         if(score<=0 || score > 999)
