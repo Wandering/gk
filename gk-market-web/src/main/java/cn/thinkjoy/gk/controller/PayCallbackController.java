4b826f214beac79826a4138c61b780cbe612fbcc
liuxiaowen <liuxiaowen@thinkjoy.cn>
2016-04-26 15:48:04 +0800

    支付接口fix

--- gk-market-web/src/main/java/cn/thinkjoy/gk/controller/PayCallbackController.java
+++ gk-market-web/src/main/java/cn/thinkjoy/gk/controller/PayCallbackController.java
@@ -2,7 +2,8 @@ package cn.thinkjoy.gk.controller;
 
 import cn.thinkjoy.gk.common.ZGKBaseController;
 import cn.thinkjoy.gk.constant.SpringMVCConst;
-import cn.thinkjoy.gk.service.IUserVipService;
+import cn.thinkjoy.gk.domain.Orders;
+import cn.thinkjoy.gk.service.IOrdersService;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
@@ -14,8 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by jzli on 15/6/3.
@@ -28,56 +30,46 @@ public class PayCallbackController extends ZGKBaseController {
     private static final Logger LOGGER= LoggerFactory.getLogger(PayCallbackController.class);
 
     @Autowired
-    private IUserVipService userVipService;
+    private IOrdersService ordersService;
 
     @RequestMapping(value = "payCallback", method = RequestMethod.GET)
-    public String payCallback(HttpServletRequest request, HttpServletResponse response) {
+    public String payCallback(HttpServletRequest request) {
+        String returnUrl = "www.zhigaokao.cn";
         try {
             request.setCharacterEncoding("UTF-8");
 
             BufferedReader reader = request.getReader();
             StringBuilder builder = new StringBuilder();
 
-            String s = null;
+            String s;
             while ((s = reader.readLine()) != null) {
                 builder.append(s);
             }
             reader.close();
 
-//            UserAccountPojo userAccountPojo = getUserAccountPojo();
-//            if(userAccountPojo==null){
-//                throw new BizException(ERRORCODE.NO_LOGIN.getCode(),ERRORCODE.NO_LOGIN.getMessage());
-//            }
-//            long userId=userAccountPojo.getId();
-//            UserVip userVip = new UserVip();
-//            userVip.setId(userId);
-//            userVip.setStatus(2);
-//            userVipService.update(userVip);
-//
-//            userAccountPojo.setVipStatus(2);
-//
-//            setUserAccountPojo(userAccountPojo);
-
             JSONObject payResult = JSON.parseObject(builder.toString());
 
             LOGGER.info("====pay /payCallback payResult: "+payResult);
 
+            if( null != payResult) {
+                long orderNo = payResult.getLong("orderNo");
+                returnUrl = payResult.getString("returnUrl");
+                Map<String,Object> dataMap = new HashMap();
+                dataMap.put("orderNo", orderNo);
+                Orders order =(Orders) ordersService.queryOne(dataMap);
+                if(order !=null&&order.getPayStatus()==0){
+                    Orders update = new Orders();
+                    update.setId(orderNo);
+                    update.setPayStatus(1);
+                    update.setStatus(1);
+                    update.setLastModDate(System.currentTimeMillis());
+                    ordersService.update(update);
+                }
+            }
         } catch (Exception e) {
             LOGGER.error("error",e);
         }
-        return "user/vip-service";
+        return "redirect:"+ returnUrl;
     }
 
-//    public static void main(String[] args) {
-//        Calendar c = Calendar.getInstance();
-//
-////        System.out.println(month);
-////        c.setTimeInMillis(System.currentTimeMillis());
-//        c.add(Calendar.YEAR, 1);
-//        c.set(Calendar.MONTH,8);
-//        c.set(Calendar.DAY_OF_MONTH,1);
-//        System.out.println(DateUtil.DateToString(new Date(System.currentTimeMillis()), DateStyle.YYYY_MM_DD_HH_MM_SS_EN));
-//        System.out.println(DateUtil.DateToString(new Date(c.getTimeInMillis()), DateStyle.YYYY_MM_DD_HH_MM_SS_EN));
-//
-//    }
 }
