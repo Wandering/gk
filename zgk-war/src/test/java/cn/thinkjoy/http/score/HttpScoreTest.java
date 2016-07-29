f1507f7912d2be818d413c0c2539b809f576e56a
杨永平 <ypyang@thinkjoy.cn>
2016-07-29 18:10:17 +0800

    本次提交内容:          成绩分析相关接口

---
+++ zgk-war/src/test/java/cn/thinkjoy/http/score/HttpScoreTest.java
@@ -0,0 +1,134 @@
+package cn.thinkjoy.http.score;
+
+import cn.thinkjoy.common.RequestUtils;
+import junit.framework.TestCase;
+
+import java.net.URL;
+import java.net.URLEncoder;
+
+/**
+ * Created by admin on 2016/1/14.
+ */
+public class HttpScoreTest extends TestCase{
+//        String host="http://10.254.130.33:9080";
+//        String host="http://10.136.56.195:8080";
+//        String host="http://zjtest.zhigaokao.cn";
+        String host="http://localhost:8080";
+        String base="/score/";
+
+
+        String queryScoreRecordByUserId_url=base+"queryScoreRecordByUserId.do";
+        String insertScoreRecord_url=base+"insertScoreRecord.do";
+        String queryInfoByRecordId_url=base+"queryInfoByRecordId.do";
+        String queryAllRecordByUserId_url=base+"queryAllRecordByUserId.do";
+        String queryBatchLineByAreaId_url=base+"queryBatchLineByAreaId.do";
+        String recommendSchool_url=base+"recommendSchool.do";
+        String queryBatchsBySchoolIdAndAreaId_url=base+"queryBatchsBySchoolIdAndAreaId.do";
+        String queryGapBySchoolIdAndBatch_url=base+"queryGapBySchoolIdAndBatch.do";
+        String insertUserInfo_url=base+"insertUserInfo.do";
+        String queryHighSchoolByCountyId_url=base+"queryHighSchoolByCountyId.do";
+        String queryUserInfo_url=base+"queryUserInfo.do";
+
+
+
+    public void testInsertUserInfo(){
+        String url=host + insertUserInfo_url + "?userId=1&provinceId=610000&cityId=610100&countyId=610102" +
+                "&schoolCode=6101024000&schoolName="+ URLEncoder.encode("西安二十九中")+"&gradeInfo="+URLEncoder.encode("高三")+"&classInfo="+URLEncoder.encode("一班")+"" +
+                "&source=1\n";
+        String result = RequestUtils.requestPost(url);
+        System.out.println("result = " + result);
+
+    }
+    public void testQueryHighSchoolByCountyId(){
+        String url=host + queryHighSchoolByCountyId_url + "?countyId=110101&schoolName=\n";
+        String result = RequestUtils.requestGet(url);
+        System.out.println("result = " + result);
+
+    }
+    public void testQueryUserInfo(){
+        String url=host + queryUserInfo_url + "?userId=1\n";
+        String result = RequestUtils.requestGet(url);
+        System.out.println("result = " + result);
+
+    }
+
+
+
+
+
+
+
+    public void testInsertScoreRecord(){
+        String url=host + insertScoreRecord_url + "?userId=1&source=1&areaId=&majorType=&scores\n";
+        String result = RequestUtils.requestPost(url);
+        System.out.println("result = " + result);
+
+    }
+
+    /**
+     * ok
+     */
+     public void testQueryScoreRecordByUserId(){
+         String url=host + queryScoreRecordByUserId_url + "?userId=1\n";
+         String result = RequestUtils.requestGet(url);
+         System.out.println("result = " + result);
+
+     }
+
+
+    /**
+     * ok
+     */
+    public void testQueryInfoByRecordId(){
+        String url=host +queryInfoByRecordId_url + "?recordId=1";
+        String result = RequestUtils.requestGet(url);
+        System.out.println("result = " + result);
+    }
+
+    /**
+     * ok
+     */
+    public void testQueryAllRecordByUserId(){
+        String url=host +queryAllRecordByUserId_url + "?userId=1&source=1";
+        String result = RequestUtils.requestGet(url);
+        System.out.println("result = " + result);
+    }
+
+    /**
+     * ok
+     */
+    public void testQueryBatchLineByAreaId(){
+        String url=host +queryBatchLineByAreaId_url + "?totalScore=460&areaId=610000&majorType=2";
+        String result = RequestUtils.requestGet(url);
+        System.out.println("result = " + result);
+    }
+
+    /**
+     * ok
+     */
+    public void testQueryGapBySchoolIdAndBatch(){
+        String url=host +queryGapBySchoolIdAndBatch_url + "?recordId=1&schoolId=1&batch=1&userId=1&source=1";
+        String result = RequestUtils.requestPost(url);
+        System.out.println("result = " + result);
+    }
+
+    /**
+     * ok
+     */
+    public void testQueryBatchsBySchoolIdAndAreaId(){
+        String url=host +queryBatchsBySchoolIdAndAreaId_url + "?areaId=610000&schoolId=1";
+        String result = RequestUtils.requestGet(url);
+        System.out.println("result = " + result);
+    }
+
+    /**
+     * ok
+     */
+    public void testRecommendSchool(){
+        String url=host +recommendSchool_url + "?totalScore=500&areaId=610000&majorType=1";
+        String result = RequestUtils.requestGet(url);
+        System.out.println("result = " + result);
+    }
+
+
+}
