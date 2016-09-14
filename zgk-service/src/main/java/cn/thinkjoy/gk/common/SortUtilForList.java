5ecf1dce13d6282f0e38d53a54180d335951d223
杨永平 <ypyang@thinkjoy.cn>
2016-09-14 15:45:27 +0800

    1、解决通用技术推荐院校过少 2、解决辽宁院校没有录取信息的问题

--- zgk-service/src/main/java/cn/thinkjoy/gk/common/SortUtilForList.java
+++ zgk-service/src/main/java/cn/thinkjoy/gk/common/SortUtilForList.java
@@ -25,55 +25,15 @@ public class SortUtilForList implements Comparator<Map<String,Object>> {
         Integer isCurrArea_o1=o1.get("isCurrArea").toString() == null?null:Integer.parseInt(o1.get("isCurrArea").toString());
         Integer isCurrArea_o2=o2.get("isCurrArea").toString() == null?null:Integer.parseInt(o2.get("isCurrArea").toString());
 
-        Integer isFavorite_o1=o1.get("isFavorite").toString() == null?null:Integer.parseInt(o1.get("isCurrArea").toString());
-        Integer isFavorite_o2=o2.get("isFavorite").toString() == null?null:Integer.parseInt(o2.get("isCurrArea").toString());
+        Integer isFavorite_o1=o1.get("isFavorite").toString() == null?null:Integer.parseInt(o1.get("isFavorite").toString());
+        Integer isFavorite_o2=o2.get("isFavorite").toString() == null?null:Integer.parseInt(o2.get("isFavorite").toString());
 
-        if (isCurrArea_o1!=isCurrArea_o2) {
-            return compareWithCurrArea(isCurrArea_o1,isCurrArea_o2);
-        } else if (isFavorite_o1!=isFavorite_o2) {
-            return compareWithFavorite(isFavorite_o1,isFavorite_o2);
-        } else if (enrollRate_o1!=enrollRate_o2) {
-            return compareWithEnrollRate(enrollRate_o1,enrollRate_o2);
-        }
-        return 0;
+        Integer result=isCurrArea_o2.compareTo(isCurrArea_o1);
+        if(result==0)
+            result=isFavorite_o2.compareTo(isFavorite_o1);
+        if(result==0)
+            result=enrollRate_o1.compareTo(enrollRate_o2);
+        return  result;
     }
 
-    /**
-     * 通过学生的学号进行排序
-     * @param id1
-     * @param id2
-     * @return
-     */
-    private int compareWithEnrollRate(int id1, int id2) {
-        if (id1 > id2){
-            return -1;
-        }
-        return 1;
-    }
-
-    /**
-     * 通过学生的分数进行排序
-     * @param score1
-     * @param score2
-     * @return
-     */
-    private int compareWithCurrArea(int score1, int score2) {
-        if (score1 > score2){
-            return -1;
-        }
-        return 1;
-    }
-
-    /**
-     * 通过学生的分数进行排序
-     * @param score1
-     * @param score2
-     * @return
-     */
-    private int compareWithFavorite(int score1, int score2) {
-        if (score1 > score2){
-            return -1;
-        }
-        return 1;
-    }
 }
