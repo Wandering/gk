5e763f279fb20200a6b697752d7b0eadfc3fb408
yangyongping <yangyongping@operationadmins-MacBook-Pro.local>
2016-11-25 20:43:59 +0800

    设置提前时间

--- expert-admin-service/src/main/java/cn/thinkjoy/gk/service/impl/ExpertOrderServiceImpl.java
+++ expert-admin-service/src/main/java/cn/thinkjoy/gk/service/impl/ExpertOrderServiceImpl.java
@@ -86,6 +86,7 @@ public class ExpertOrderServiceImpl extends AbstractPageService<IBaseDAO<ExpertR
         expertOrderDTO.setCustomerId(detailDTO.getUserId());
         expertOrderDTO.setServiceTime(detailDTO.getExpectTime());
         expertOrderDTO.setServiceId(detailDTO.getServiceItem());
+        expertOrderDTO.setIsInto(detailDTO.getIsInto());
         return expertOrderDTO;
     }
 
@@ -157,7 +158,7 @@ public class ExpertOrderServiceImpl extends AbstractPageService<IBaseDAO<ExpertR
                     //结束
                     status = ExpertAdminConst.EXPERT_ORDER_STATUS_Y3;
                 }
-                if (lStartTime-tqTime-currTime<0 && lEndTime-currTime>0){
+                if (lStartTime-tqTime-currTime<0 && lEndTime-currTime>0&& "智高考网站".equals(expertReservationOrderDetailDTO.getChannel())){
                     expertReservationOrderDetailDTO.setIsInto(2);
                 }else {
                     expertReservationOrderDetailDTO.setIsInto(1);
